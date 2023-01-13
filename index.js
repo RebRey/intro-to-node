b9b989d74b3f0e4ba9a00a99dbd0d2eb37256cc1
Rebecca Reyes <rebreyaz@gmail.com>
2023-01-12 23:59:12 -0700

    index.js update

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-//To CommonJS syntax to use the callback and sync APIs:
+//use CommonJS syntax to use the callback and sync APIs:
 const fs = require('node:fs');
 
 // fs.copyFile(src, dest[, mode], callback)
-
+// src <string> | <Buffer> | <URL> source filename to copy
+// dest <string> | <Buffer> | <URL> destination filename of the copy operation
+// mode <integer> modifiers for copy operation. Default: 0.
+// callback <Function>
 fs.copyFile("file1.txt", "file2.txt", messageFunction);
 function messageFunction() {
     console.log("file 1 was copied");
